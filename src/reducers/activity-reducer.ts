7eb8692474909242c68aab01b1510d22b136319d
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-22 20:08:35 -0300

    reducer activity

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,16 @@
+import { Activity } from "../types"
+export type ActivityActions = {
+
+}
+
+type ActivityState = {
+    activities: Activity[]
+}
+
+export const initialState : ActivityState ={
+    activities: []
+}
+
+export const activityReducer = () => {
+
+}
