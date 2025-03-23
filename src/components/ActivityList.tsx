90c18b3627d5b6ae04862633ba9a05988e77c959
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-23 14:46:15 -0300

    local storage

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -11,38 +11,41 @@ export type ActivityListProps = {
 export default function ActivityList({activities, dispatch} : ActivityListProps){
 
     const categoryName = useMemo(() => (category : Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities] )
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
     return (
        <>
         <h2 className="text-4xl font-bold text-center text-slate-600">Comida y Actividades</h2>
-        {activities.map(activity => (
-            <div key={activity.id} className="px-5 py-10 bg-lime-100 mt-5 flex justify-between rounded-lg">
-                <div className="space-y-2 relative">
-                    <p className={`absolute -top-5 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
-                        {categoryName(activity.category)}
-                    </p>
-                    <p className="text-2xl font-bold pt-5">
-                        {activity.name}
-                    </p>
-                    <p className="font-black text-4xl text-lime-500">
-                        {activity.calories} {''}
-                        <span>Calorias</span>
-                    </p>
-                </div>
-                <div className="flex items-center gap-5">
-                    <button 
-                        onClick={() => dispatch({type: 'set-active-id', payload: {activeId: activity.id}})}
-                    >
-                        <PencilSquareIcon className="w-6 h-6 text-slate-800 cursor-pointer" />
-                    </button>
-                    <button
-                        onClick={() => dispatch({type: 'delete-activity', payload: {activeId: activity.id}})}
-                    >
-                        <TrashIcon className="w-6 h-6 text-slate-800 cursor-pointer" />
-                    </button>
 
+        {isEmptyActivities ? <p className="text-center text-slate-600 my-5">No hay actividades aún...</p>: 
+            activities.map(activity => (
+                <div key={activity.id} className="px-5 py-10 bg-lime-100 mt-5 flex justify-between rounded-lg">
+                    <div className="space-y-2 relative">
+                        <p className={`absolute -top-5 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                            {categoryName(activity.category)}
+                        </p>
+                        <p className="text-2xl font-bold pt-5">
+                            {activity.name}
+                        </p>
+                        <p className="font-black text-4xl text-lime-500">
+                            {activity.calories} {''}
+                            <span>Calorias</span>
+                        </p>
+                    </div>
+                    <div className="flex items-center gap-5">
+                        <button 
+                            onClick={() => dispatch({type: 'set-active-id', payload: {activeId: activity.id}})}
+                        >
+                            <PencilSquareIcon className="w-6 h-6 text-slate-800 cursor-pointer" />
+                        </button>
+                        <button
+                            onClick={() => dispatch({type: 'delete-activity', payload: {activeId: activity.id}})}
+                        >
+                            <TrashIcon className="w-6 h-6 text-slate-800 cursor-pointer" />
+                        </button>
+
+                    </div>
                 </div>
-            </div>
-        ))}
+            ))} 
        </>
     )
 }
