cf9162c871c51e91a6d11cc8a0c0e706b13578ed
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-23 19:18:01 -0300

    diferencia de calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay"
 
 type CalorieTrackerProps = {
     activities: Activity[]
@@ -10,20 +11,25 @@ export default function CalorieTracker({activities}: CalorieTrackerProps){
     //contadores
     const caloriesConsumed = useMemo(() => activities.reduce((acc, activity) => activity.category === 1 ? acc + activity.calories : acc, 0), [activities])
     const caloriesBurned = useMemo(() => activities.reduce((acc, activity) => activity.category === 2 ? acc + activity.calories : acc, 0), [activities])
+    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities])
 
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
 
             <div className="flex flex-col items-center md:justify-between md:flex-row gap-5 mt-10">
-                <p className="text-white text-center font-bold rounded-full grid grid-cols-1 gap-3">
-                    <span className="font-black text-6xl text-orange-500">{caloriesConsumed}</span>
-                    Consumidas
-                </p>
-                <p className="text-white text-center font-bold rounded-full grid grid-cols-1 gap-3">
-                    <span className="font-black text-6xl text-orange-500">{caloriesBurned}</span>
-                    Quemadas
-                </p>
+                <CalorieDisplay 
+                    calories={caloriesConsumed} 
+                    text="Consumidas" 
+                />
+                <CalorieDisplay 
+                    calories={caloriesBurned} 
+                    text="Quemadas" 
+                />
+                <CalorieDisplay 
+                    calories={netCalories} 
+                    text="Diferencia" 
+                />
             </div>
         </>
     )
